2eefaefcf98e5402f65e113508a64cc0b5e0afbb
KayDjay <Khadijatrasaq@gmail.com>
2024-04-24 15:17:18 +0100

    Amended user table

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer,String
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 
 class User(Base):
+    """
+    Represents a user in the system.
+
+    Attributes:
+        id (int): The unique identifier for the user.
+        email (str): The email address of the user.
+        hashed_password (str): The hashed password of the user.
+        session_id (str): The session ID of the user.
+        reset_token (str): The reset token for the user's password reset.
+    """
+
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
