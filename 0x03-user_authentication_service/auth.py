cdabd9d10be6081164704ca124cc5dd36f2846df
KayDjay <Khadijatrasaq@gmail.com>
2024-04-26 01:25:03 +0100

    Hash Password task

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env pthon3
+"""
+Auth Model
+"""
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """Hashes the input password with bcrypt.
+
+    Args:
+        password (str): The password string to be hashed.
+
+    Returns:
+        bytes: A salted hash of the input password.
+    """
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed
